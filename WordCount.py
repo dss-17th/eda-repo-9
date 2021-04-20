858d48635727fead7f9b9480022de0089587e03d
solyourock <greatsol98@gmail.com>
2021-04-20 19:26:46 +0900

    python module for eda

---
+++ WordCount.py
@@ -0,0 +1,43 @@
+
+def WordCount(df, column_name, delval="정보없음", column_name1="영화명",rank=20):
+    '''
+    df is input dataframe
+    column_name is which columns you want to drop
+    delval is value that you want to delete
+    column_name1 is which columns you want to see Rank word data
+    rank Rank you want to see 
+    '''
+    # import,load stopword
+    import nltk
+    from konlpy.tag import Okt; t = Okt()
+    import pandas as pd
+    import seaborn as sns
+    from DeleteVal import DeleteVal
+    from WordCount import WordCount
+    import matplotlib.pylab as plt
+    stop_words = pd.read_csv('한국어불용어100.txt', sep = "\t", engine='python')["stop"]
+    stop_words = list(stop_words)
+    
+    # DeleteVal from columns and filter by columns1
+    word_column = DeleteVal(df, column_name)[column_name1]
+    
+    # text data
+    present_text = ''
+    for each_line in word_column:
+        present_text = present_text + each_line + '\n'
+    
+    # okt and nltk and make Rank data
+    tokens_ko = t.nouns(present_text)
+    tokens_ko = [each_word for each_word in tokens_ko if each_word not in stop_words]
+    ko = nltk.Text(tokens_ko)
+    Rank_data = ko.vocab().most_common(rank)
+    
+    # graph
+    x, y = dict(Rank_data).keys(), dict(Rank_data).values()
+    plt.figure(figsize=(15,8))
+    plt.title(f"자주 쓰인 단어 {rank}개", fontsize=20)
+    sns.barplot(list(x), list(y), palette="Dark2")
+    plt.show()
+    
+    # return Rank_data for next use 
+    return Rank_data
