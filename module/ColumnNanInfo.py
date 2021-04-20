8ab4648eaa34e727d256c641a54f6d49a2ce69f4
FLY-CODE77 <lightyear94122@gmail.com>
2021-04-20 19:38:00 +0900

    File update

---
+++ module/ColumnNanInfo.py
@@ -0,0 +1,10 @@
+def ColumnNanInfo(df, column_name, rval=2):
+    '''
+    df is input dataframe 
+    column_name is which columns want to see the nan value
+    rval is round value
+    '''
+    num1 = (df[column_name] == "정보없음").sum()
+    num2 = len(df[column_name])
+    percent = round((num1/num2) * 100, rval)
+    print(f" {column_name} 중 정보없음의 퍼센트는 {percent} 입니다")
