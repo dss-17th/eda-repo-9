9783e56a5a97d99227bc4509570f69e0cf6ffd60
solyourock <greatsol98@gmail.com>
2021-04-20 19:35:34 +0900

    file move

--- DeleteVal.py
+++
@@ -1,8 +0,0 @@
-def DeleteVal(df, column_name, delval="정보없음"):
-    '''
-    df is input dataframe
-    column_name is which columns you want to drop
-    delval is value that you want to delete
-    '''
-    df = df[df[column_name] != delval]
-    return df
