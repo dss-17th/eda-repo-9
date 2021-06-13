6bb9074e0c3f94639e78ee3928bc9c21d9636a0c
FLY-CODE77 <lightyear94122@gmail.com>
2021-06-13 17:34:24 +0900

    monthly whole movie movies number count

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from PIL import Image
 from wordcloud import ImageColorGenerator
 from pyecharts.charts import Pie
 from pyecharts import options as opts
+import matplotlib.pylab as plt
 
 from module.rate import rating
 from module.BubbleChart import BubbleChart
@@ -69,4 +70,33 @@ plt.text(1.4, 0.8, f"일반영화 작품수 {df['영화구분'].value_counts().v
 plt.axis('equal')
 plt.title('영화 구분에 따른 전체 개봉영화 작품수', fontsize=20)
 plt.savefig("./image/piechart1.png")
-plt.show()
+
+
+# 월별 전체 영화 개봉 작품수 파일 
+# 개봉일 정보 없는 데이터들은 drop
+df_open = df[df["개봉일"] != "정보없음"]
+
+# 개봉일에 따른 영화 갯수 파악을 위한 전처리 작업 
+df_open["개봉일"] = df_open["개봉일"].str.split("-").str[0] + df_open["개봉일"].str.split("-").str[1]
+df_open["개봉일"] = df_open["개봉일"].astype(int)
+df_open = df_open[df_open["개봉일"] >= 201101]
+
+# 데이트 타임으로 형 변환 후 그래프화 작업
+df_open["개봉일"] = pd.to_datetime(df_open["개봉일"], format="%Y%m")
+df_open1 = df_open[df_open["영화구분"] == "일반영화"]
+df_open2 = df_open[df_open["영화구분"] != "일반영화"]
+
+
+# 연도별 전체 영화 개봉작품 수
+plt.figure(figsize=(15,8))
+plt.rc('font',size=20)
+
+plt.plot(df_open1.groupby(df_open1["개봉일"]).size(),color = 'darkkhaki', linewidth=5, linestyle=':', label='일반영화')
+plt.plot(df_open2.groupby(df_open2["개봉일"]).size(),color ='teal', linewidth=5, label='독립/예술영화')
+
+plt.legend(loc=8) 
+plt.grid(True, axis='y')
+plt.title("월별 전체 영화 개봉작품수", fontsize=20)
+plt.xlabel('개봉일')
+plt.ylabel('영화 개수')
+plt.savefig("./image/plotChart1.png")
