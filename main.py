db1d02b8d01795b2269c70b9018e47b9b2cf603d
FLY-CODE77 <lightyear94122@gmail.com>
2021-06-13 15:55:10 +0900

    Movie Data split

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+# Module Load
 import warnings
 warnings.filterwarnings(action='ignore')
 
 from module.Font import Fontmanager
 path = Fontmanager()
 
-# module 
 import pandas as pd
 import missingno as msno
 import datetime
@@ -23,3 +23,23 @@ from module.BubbleChart import BubbleChart
 from module.ColumnNanInfo import ColumnNanInfo
 from module.DeleteVal import DeleteVal
 from module.WordCount import WordCount
+
+# DataLoad 
+df = pd.read_csv('./data/KoreaMovieData.csv', thousands = ',' )
+
+# Data Cleaning 
+df.columns = df.columns.str.replace('\n','')
+df.set_index("순번", inplace = True)
+
+# Filling missing value :0 
+df = df.fillna("정보없음")
+
+# Rank Criteria Shift 
+raking_df = []
+for i in df["등급"]:
+    raking_df.append(rating(i))
+df['등급'] = raking_df
+
+# movie type split Indie film | film
+indiefilm = df[df["영화구분"] == "독립/예술영화"]
+film = df[df["영화구분"] == "일반영화"] 
